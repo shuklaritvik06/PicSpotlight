92d35646e9b889c3b169f3ea4febf6d968158a00
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-06-20 20:50:52 +0530

    💄 add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './App.{ts,tsx,js,jsx}',
+    './screens/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
